1a260dc47897f63708d962e90137b41c7a4b1fdf
Bob Yuan <bob.yuan@yahoo.com>
2022-03-21 15:01:27 -0700

    Add web project to nx workspace

---
+++ apps/web/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ent2022-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'web';
+}
