1d3cbbd16f81f6d5bdfef6e876d6070bbd9d596e
Bob Yuan <bob.yuan@yahoo.com>
2022-04-01 18:23:02 -0700

    Clean up

--- apps/portal/src/app/app.tsx
+++ apps/portal/src/app/app.tsx
@@ -5,23 +5,10 @@ import styles from './app.module.scss';
 // import { Route, Link } from 'react-router-dom';
 
 export function App() {
-  return (
-    <>
-      {/* <NxWelcome title="portal" /> */}
-      <div className={styles['text']}>
-        Portal with Tailwind
-      </div>
-
-      {/* START: routes */}
-      {/* These routes and navigation have been generated for you */}
-      {/* Feel free to move and update them to fit your needs */}
-      {/* <br />
-      <hr />
-      <br /> */}
-      
-      
-      
-    </>
+  return (      
+    <div className={styles['text']}>
+      Portal with Tailwind
+    </div>
   );
 }
 
