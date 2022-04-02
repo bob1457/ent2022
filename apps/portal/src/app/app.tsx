5e5e7606cc10287b9520f57fad4b03ee2fc1033e
Bob Yuan <bob.yuan@yahoo.com>
2022-04-01 18:09:20 -0700

    Configure using module.scss

--- apps/portal/src/app/app.tsx
+++ apps/portal/src/app/app.tsx
@@ -1,21 +1,23 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.scss';
-import NxWelcome from './nx-welcome';
+// import NxWelcome from './nx-welcome';
 
-import { Route, Link } from 'react-router-dom';
+// import { Route, Link } from 'react-router-dom';
 
 export function App() {
   return (
     <>
-      <NxWelcome title="portal" />
-      <div />
+      {/* <NxWelcome title="portal" /> */}
+      <div className={styles['text']}>
+        Portal with Tailwind
+      </div>
 
       {/* START: routes */}
       {/* These routes and navigation have been generated for you */}
       {/* Feel free to move and update them to fit your needs */}
-      <br />
+      {/* <br />
       <hr />
-      <br />
+      <br /> */}
       
       
       
