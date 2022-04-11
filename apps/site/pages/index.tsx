42550cd581bfd2c610bc90e36aef47ee82fa48f2
Bob Yuan <bob.yuan@yahoo.com>
2022-04-11 14:03:01 -0700

    Configure nextjs use module.css

--- apps/site/pages/index.tsx
+++ apps/site/pages/index.tsx
@@ -6,11 +6,12 @@ export function Index() {
    *
    * Note: The corresponding styles are in the ./index.scss file.
    */
-  return (
+  console.log(styles);
+  return (    
     <div className="bg-gray-50">
-      <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
+      <div className={styles.home}>
         <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-          <span className="block">Ready to dive in?</span>
+          <span className={styles.title}>Ready to dive in?</span>
           <span className="block text-indigo-600">
             Start your free trial today.
           </span>
