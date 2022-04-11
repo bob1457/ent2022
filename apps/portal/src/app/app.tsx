982d537aa4c85fa5f1840fcbbe3c809f3e1a7517
Bob Yuan <bob.yuan@yahoo.com>
2022-04-11 14:42:58 -0700

    Resolved confict

--- apps/portal/src/app/app.tsx
+++ apps/portal/src/app/app.tsx
@@ -1,5 +1,4 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-<<<<<<< HEAD
 import styles from './app.module.scss';
 // import NxWelcome from './nx-welcome';
 
@@ -10,17 +9,6 @@ export function App() {
     <div className={styles['text']}>
       Portal with Tailwind
     </div>
-=======
-import styles from './app.module.css';
-import NxWelcome from './nx-welcome';
-
-export function App() {
-  return (
-    <>
-      <NxWelcome title="portal" />
-      <div />
-    </>
->>>>>>> 2a7844feb2976b2305401744444286acd6809f32
   );
 }
 
