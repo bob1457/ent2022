1f3a84274c111954beaed1bcb79acb56a7a0e17e
Bob Yuan <bob.yuan@yahoo.com>
2022-04-30 12:16:27 -0700

    Add app module auth

--- apps/web/src/app/app.module.ts
+++ apps/web/src/app/app.module.ts
@@ -5,13 +5,19 @@ import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 // import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from '@ent2022/material';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
   imports: [
     BrowserModule,
     // RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
-    AppRoutingModule
+    AuthModule,
+    AppRoutingModule,
+    BrowserAnimationsModule, 
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent],
--- apps/web/src/app/auth/auth.module.ts
+++ apps/web/src/app/auth/auth.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
+import { MaterialModule } from '@ent2022/material';
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MaterialModule
+  ],
   exports: [LoginComponent]
 })
 export class AuthModule {}
